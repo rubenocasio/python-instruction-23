a0bbc8f65016a5b0af616728e77e64c1688e6b8b
Ruben Ocasio <rocasio@rubenocasio.com>
2023-11-28 13:40:47 -0500

    Create conditionals.py

---
+++ 01-python-fundamentals/w1d1-02-conditionals/conditionals.py
@@ -0,0 +1,38 @@
+# If Statement
+# x = 9
+# if x > 8:
+    # print("x is greater than 8")
+    # print(f"{x} is greater than 8!")
+    
+# If-Else Statement:
+# x = 9
+# if x > 8:
+#     print(f"{x} is greater than 8!")
+# else:
+#     print(f"{x} is not greater than 8!")
+
+# If-Elif-Else
+# x = 9
+# if x > 10:
+#     print(f"{x} is greater than 10!")
+# elif x == 10:
+#     print(f"{x} is equal to 10!")
+# else:
+#     print(f"{x} is not greater than 10!")
+
+# Boolean Operators
+# <, >, ==, &, |, and, or, <=, >=
+is_monday = False
+is_home = True
+x = 15
+
+if is_monday and is_home:
+    print("It's Monday and I'm at home!")
+    #True       True
+if is_monday or is_home:
+    print("It's either Monday or I'm at home!")
+
+if x < 10 and x > 5:
+    print("I am Great!")
+else:
+    print("NOT!!!")
