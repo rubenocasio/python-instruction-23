b74a2d847f5eee56eab61a0cb73e967ba9f40b52
Ruben Ocasio <rocasio@rubenocasio.com>
2023-11-29 09:36:46 -0500

    Create w1d2-reverse.js

---
+++ 00-algorithms/w1/w1d2-reverse.js
@@ -0,0 +1,41 @@
+/* 
+  String: Reverse
+
+  Given a string, return a new string that is
+  the given string reversed
+*/
+
+const str1 = 'creature';
+const expected1 = 'erutaerc';
+
+const str2 = 'dog';
+const expected2 = 'god';
+
+const str3 = 'hello';
+const expected3 = 'olleh';
+
+const str4 = '';
+const expected4 = '';
+
+/**
+ * Reverses the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str String to be reversed.
+ * @returns {string} The given str reversed.
+ */
+function reverseString(str) {
+  //Logic goes here
+}
+
+const result1 = reverseString(str1);
+console.log(result1);
+
+const result2 = reverseString(str2);
+console.log(result2);
+
+const result3 = reverseString(str3);
+console.log(result3);
+
+const result4 = reverseString(str4);
+console.log(result4);
