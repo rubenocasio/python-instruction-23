144881188f4c8c44839b1e88bad92380b1b80ded
Ruben Ocasio <rocasio@rubenocasio.com>
2023-11-29 13:18:20 -0500

    @classmethods

---
+++ 02-python-oop/w1d3-00-class-methods/classes/user.py
@@ -0,0 +1,18 @@
+from classes.guitar import Guitar
+
+class User:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.guitar = Guitar("Les", "Paul")
+    
+    def display_info(self):
+        print(f"Name: {self.first_name} {self.last_name}")
+        print(f"My age is {self.age}")
+    
+    def play_guitar(self):
+        self.guitar.play()
+    
+    def stop_guitar(self):
+        self.guitar.stop_play()
