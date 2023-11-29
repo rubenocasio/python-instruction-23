0b3b00d6e47c745aa39997e144a8b135cf1c3735
Ruben Ocasio <rocasio@rubenocasio.com>
2023-11-29 11:54:02 -0500

    Update w1d2-reverse.js

--- 00-algorithms/w1/w1d2-reverse.js
+++ 00-algorithms/w1/w1d2-reverse.js
@@ -25,7 +25,22 @@ const expected4 = '';
  * @returns {string} The given str reversed.
  */
 function reverseString(str) {
-  //Logic goes here
+  let reversed = ''
+
+  //Vanilla for loop
+  // for(let i = 0; i < str.length; i++){
+  //   console.log(i)
+  // }
+
+  for(let i = str.length - 1; i >= 0; i--){
+    // console.log(i)
+    // console.log(str[char])
+    // console.log("This is my " + str[char] )
+        //actual letter [index value]
+    reversed += str[i]
+    // console.log("This is reversed " + reversed )
+  }
+  return reversed;
 }
 
 const result1 = reverseString(str1);
@@ -37,5 +52,5 @@ console.log(result2);
 const result3 = reverseString(str3);
 console.log(result3);
 
-const result4 = reverseString(str4);
-console.log(result4);
+// const result4 = reverseString(str4);
+// console.log(result4);
