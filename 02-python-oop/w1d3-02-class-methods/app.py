c080e18a94d1cafc719be26820b4bf74801322aa
Ruben Ocasio <rocasio@rubenocasio.com>
2023-11-29 17:12:52 -0500

    Create app.py

---
+++ 02-python-oop/w1d3-02-class-methods/app.py
@@ -0,0 +1,30 @@
+#Path to file
+from classes.guitar import Guitar
+from classes.user import User
+
+ruben = Guitar("Gibson", "Strato", 8)
+# billy = Guitar("Martin", "Strat", 8)
+# tyler = Guitar("Les Paul", "X", 8)
+# kyle = Guitar("Fender", "Stratocaster", 8)
+
+cihan = User("Cihan", "Akbaba", 21)
+
+# ruben.play().stop_play()
+# ruben.change_strings(25)
+# Guitar.num_of_guitars
+# print(Guitar.num_of_guitars)
+
+cihan.play_guitar()
+cihan.guitar.play()
+
+# print(ruben.num_of_guitars())
+
+
+# billy.play().stop_play()
+# billy.change_strings(19)
+
+# tyler.play().stop_play()
+# tyler.change_strings(9)
+
+# kyle.play().stop_play()
+# kyle.change_strings(1)
