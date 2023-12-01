0a8241bf002f797b4b12d0042e37476b3542645a
Ruben Ocasio <rocasio@rubenocasio.com>
2023-12-01 10:58:52 -0500

    Flask Routing

--- 03-Flask/w1d4-02-routing/flask-routing/server.py
+++ 03-Flask/w1d4-02-routing/flask-routing/server.py
@@ -1,20 +1,38 @@
 # Import Flask to allow us to create our app
-from flask import Flask
+from flask import Flask, render_template
 
 # Create a new instance of the Flask class called "app"
 app = Flask(__name__)
 
 # The "@" decorator associates this route with the function immediately following
-# / means root
+# '/' means root
 @app.route('/')
 def index():
     # Return the string 'Hello World!' as a response
     return 'Hello World!'
 
-@app.route('/<name>')
+@app.route('/home/<name>')
 def greet_name(name):
     # Return the string 'Hello World!' as a response
     return f'<h1>Hello, {name} Welcome to Flask <h1>'
+
+@app.route('/home/index')
+def home():
+    return render_template('index.html')
+
+@app.route('/home/students')
+def students():
+    students = [
+        {'name': 'Kyle', 'age': 21},
+        {'name': 'Kevin', 'age': 21},
+        {'name': 'Brooks', 'age': 21},
+    ]
+    name = 'Ruben'
+    return render_template('students.html', some_name = name, students = students )
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("error.html", error = error)
 
 # Ensure this file is being run directly and not from a different module    
 if __name__=="__main__":
---
+++ 03-Flask/w1d5-04-sessions/post-session-demo/server.py
@@ -0,0 +1,5 @@
+# 1. mkdir post-session-demo
+# 2. cd post-session-demo
+# 3. pip install pipenv (One time install)
+# 4. pipenv install flask (whats in your env run: pip list)
+# 5. python server.py
