c12fbb2c7ebd053ca78c0b8315bfcfd5bd0d92ae
Ruben Ocasio <rocasio@rubenocasio.com>
2023-12-01 13:28:43 -0500

    Session Demo

--- 03-Flask/w1d5-04-sessions/post-session-demo/server.py
+++ 03-Flask/w1d5-04-sessions/post-session-demo/server.py
@@ -1,5 +1,42 @@
 # 1. mkdir post-session-demo
 # 2. cd post-session-demo
 # 3. pip install pipenv (One time install)
-# 4. pipenv install flask (whats in your env run: pip list)
-# 5. python server.py
+# 4. pipenv shell (Virtual Environment)
+# 5. pipenv install flask (whats in your env run: pip list)
+# 6. python server.py
+
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "You can name this anything"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    print(request.form)
+
+    session['account'] = request.form['account']
+    session['name'] = request.form['name']
+    return redirect('/display')
+
+@app.route('/display')
+def display():
+    print("This is my display route")
+    
+    # if 'account' in session:
+    #     account = session['account']
+    # else:
+    #     account = 'Nothing here to look at'
+    # return render_template('display.html', account = account)
+    return render_template('display.html')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+if __name__=="__main__":
+    app.run(debug=True)   
