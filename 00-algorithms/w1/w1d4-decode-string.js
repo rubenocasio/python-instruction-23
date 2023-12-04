dae7ae45c03655d16d83645fc38a7160c0d6eca3
Ruben Ocasio <rocasio@rubenocasio.com>
2023-12-04 09:19:14 -0500

    Update w1d4-decode-string.js

--- 00-algorithms/w1/w1d4-decode-string.js
+++ 00-algorithms/w1/w1d4-decode-string.js
@@ -19,7 +19,45 @@ const expected2 = 'aaabbccccccccccccdddddddddd';
  * @returns {string} The given str decoded / expanded.
  */
 function decodeStr(str) {
-  //Logic goes here
+  //Initialize an empty string to hold our decoded results
+  let decoded = '';
+
+  //Create a variable to keep track of the current char that is being processed
+  let currentChar = '';
+
+  for(let char = 0; char < str.length; char++){
+    //Initialize an empty string to build a number string
+    let digits = '';
+    
+    //Check if the current char is not a number
+    if(isNaN(str[char])){
+      
+      //Set the current char
+      currentChar = str[char];
+      // console.log(currentChar)
+      
+      //Move to the next char
+      char++
+      
+      //Loop through digits after the current char
+      while(!isNaN(str[char])){
+        //Append the digit to the digits string
+        digits += str[char]
+        // console.log(digits)
+        
+        //move to the next char
+        char++
+      }
+    }
+    
+    for (let i = 1; i <= parseInt(digits); i++){
+      decoded += currentChar
+    }
+    char--
+  }
+
+  //return our empty string
+  return decoded
 }
 
 result1 = decodeStr(str1);
