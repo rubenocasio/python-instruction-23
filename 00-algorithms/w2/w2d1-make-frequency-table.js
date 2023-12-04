734068a6f31ec15b94358aa9e787d57666340076
Ruben Ocasio <rocasio@rubenocasio.com>
2023-12-04 13:32:21 -0500

    Update w2d1-make-frequency-table.js

--- 00-algorithms/w2/w2d1-make-frequency-table.js
+++ 00-algorithms/w2/w2d1-make-frequency-table.js
@@ -4,18 +4,10 @@
 */
 
 const arr1 = ['a', 'a', 'a'];
-const expected1 = {
-  a: 3,
-};
+const expected1 = { a: 3};
 
 const arr2 = ['a', 'b', 'a', 'c', 'B', 'c', 'c', 'd'];
-const expected2 = {
-  a: 2,
-  b: 1,
-  c: 3,
-  B: 1,
-  d: 1,
-};
+const expected2 = {a: 2, b: 1, c: 3, B: 1, d: 1,};
 
 const arr3 = [];
 const expected3 = {};
@@ -29,7 +21,23 @@ const expected3 = {};
  *    from the given arr and the values are the amnt of times that item occurs.
  */
 function makeFrequencyTable(arr) {
-    //Logic goes here
+  // Create an empty object to store frequency of each element.
+  const freqTable = {};
+
+  // Iterate over each element in the array.
+  for (const elem of arr) {
+      // Check if the element is not already a key in the freqTable.
+      if (!freqTable.hasOwnProperty(elem)) {
+          // If the element is not in the freqTable, add it with a count of 1.
+          freqTable[elem] = 0;
+      } else {
+          // If the element already exists in the table, increment its count.
+          freqTable[elem] += 1;
+      }
+  }
+
+  // Return the completed frequency table.
+  return freqTable;
 }
 
 const result1 = makeFrequencyTable(arr1);
