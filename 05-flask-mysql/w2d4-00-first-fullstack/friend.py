344cb439acb6f5baa2c44941ee6d89603902f927
Ruben Ocasio <rocasio@rubenocasio.com>
2023-12-06 17:40:54 -0500

    Friends CRUD

--- 05-flask-mysql/w2d4-00-first-fullstack/friend.py
+++ 05-flask-mysql/w2d4-00-first-fullstack/friend.py
@@ -16,15 +16,51 @@ class Friend:
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM friends;"
-
         # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL(DATABASE).query_db(query)
-
         # Create an empty list to append our instances of friends
         friends = []
-        
         # Iterate over the db results and create instances of friends with cls.
         for friend in results:
             friends.append( cls(friend) )
         return friends
             
+#Create route
+    @classmethod
+    def create(cls, form_data):
+        query = """
+        INSERT INTO friends (first_name, last_name, occupation) 
+        VALUES (%(first_name)s, %(last_name)s, %(occupation)s);
+        """
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL(DATABASE).query_db(query, form_data)
+        return results
+
+#Details route & Edit route (view only)
+    @classmethod
+    def get_one(cls, friend_id):
+        query = "SELECT * FROM friends WHERE id = %(friend_id)s;"
+        data = {"friend_id" : friend_id}
+        results = connectToMySQL(DATABASE).query_db(query, data)
+        friend =  Friend(results[0])
+        return friend
+
+#Update route
+    @classmethod
+    def update(cls, form_data):
+        query = """
+        UPDATE friends SET first_name = %(first_name)s, last_name = %(last_name)s, occupation = %(occupation)s 
+        WHERE id = %(friend_id)s;
+        """
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        # results = connectToMySQL(DATABASE).query_db(query, form_data)
+        # return results
+        connectToMySQL(DATABASE).query_db(query, form_data)
+
+#Delete route
+    @classmethod
+    def delete(cls, friend_id):
+        query = "DELETE FROM friends WHERE id = %(friend_id)s;"
+        data = {"friend_id" : friend_id}
+        connectToMySQL(DATABASE).query_db(query, data)
+        return
--- 05-flask-mysql/w2d4-00-first-fullstack/server.py
+++ 05-flask-mysql/w2d4-00-first-fullstack/server.py
@@ -8,17 +8,45 @@ from friend import Friend
 @app.route('/')
 def index():
     friends = Friend.get_all()
-
-    print(friends[0].first_name)
-
+    pprint(friends[0].first_name)
     return render_template('index.html', friends = friends)
 
 ## MORE ROUTES Below 
 
+# View route to open new form page
 @app.route("/friends/new")
 def new_friend():
     return render_template("new.html")
 
+#Create route
+@app.post("/friends/create")
+def create_friend():
+    friends = Friend.create(request.form)
+    return redirect("/")
+
+#Details route
+@app.route("/friends/<int:friend_id>")
+def friend_details(friend_id):
+    friend = Friend.get_one(friend_id)
+    return render_template("details.html", friend = friend)
+
+#Edit route (view only)
+@app.route("/friends/<int:friend_id>/edit")
+def edit_friend(friend_id):
+    friend = Friend.get_one(friend_id)
+    return render_template("edit.html", friend = friend)
+
+#Update route
+@app.post("/friends/<int:friend_id>/update")
+def update_friend(friend_id):
+    Friend.update(request.form)
+    return redirect(f"/friends/{friend_id}")
+
+#Delete route
+@app.post("/friends/<int:friend_id>/delete")
+def delete_friend(friend_id):
+    Friend.delete(friend_id)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
