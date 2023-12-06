f750f1b31e8123c7e38dba9dd28f13069f50d2d2
Ruben Ocasio <rocasio@rubenocasio.com>
2023-12-06 15:50:25 -0500

    First Full Stack + ERD

--- 05-flask-mysql/w2d4-00-first-fullstack/server.py
+++ 05-flask-mysql/w2d4-00-first-fullstack/server.py
@@ -1,5 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, session
+from pprint import pprint
 app = Flask(__name__)
+
+from friend import Friend
+
+# The "@" decorator associates this route with the function immediately following
+@app.route('/')
+def index():
+    friends = Friend.get_all()
+
+    print(friends[0].first_name)
+
+    return render_template('index.html', friends = friends)
+
+## MORE ROUTES Below 
+
+@app.route("/friends/new")
+def new_friend():
+    return render_template("new.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
